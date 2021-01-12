ba04a3fcb145551952396cbcf6737676a0056e56
Kitty BRANCOURT <k.brancourt@gmail.com>
2021-01-12 17:01:13 +0100

    exercice 32

---
+++ exercice33/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        $.ajax({
+            url: 'https://restcountries.eu/rest/v2/name/france',
+            success: function (value) {
+                console.log(value[0].name);
+                console.log(value[0].capital);
+            }
+        });
+    })
+})
